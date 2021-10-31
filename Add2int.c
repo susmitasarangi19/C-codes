bd1cda5c36834c35e5f6f5724fc427512fcbabd5
Dhanendra Kumar Sonber <dhanendrakumarsonber@gmail.com>
2021-10-31 20:00:32 +0530

    Assigning more comments for easy understanding
    Program is ok but need more comments.....

--- Add2int.c
+++ Add2int.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-int main() {    
-
-    int number1, number2, sum;
-    
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculating sum
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
+// C program to add two integers
+#include<stdio.h>
+  
+int main()
+{
+    int A, B, sum = 0;
+      
+    //Enter the two numbers
+    printf("Enter two numbers A and B : \n");
+      
+    // Reading two numbers from the user
+    scanf("%d%d", &A, &B);
+      
+    // Calclulating the addition of A and B
+    // using '+' operator
+    sum = A + B;
+      
+    // Print the sum
+    printf("Sum of A and B is: %d", sum);
+      
     return 0;
 }
